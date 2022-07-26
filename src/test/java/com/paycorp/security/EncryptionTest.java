975e20b06f13bbc6d718495c8ac4f579c3fe9401
Prashant <prashant.maroli@evolvussolutions.com>
2022-07-26 11:38:14 +0400

    Added tests

---
+++ src/test/java/com/paycorp/security/EncryptionTest.java
@@ -0,0 +1,15 @@
+package com.paycorp.security;
+
+import org.junit.Test;
+
+public class EncryptionTest {
+    @Test
+    public void testDecrypt() {
+
+    }
+
+    @Test
+    public void testEncrypt() {
+
+    }
+}
---
+++ src/test/java/com/paycorp/security/XMLSignVerifyTest.java
@@ -0,0 +1,15 @@
+package com.paycorp.security;
+
+import org.junit.Test;
+
+public class XMLSignVerifyTest {
+    @Test
+    public void testSignXML() {
+
+    }
+
+    @Test
+    public void testVerifyXML() {
+
+    }
+}
