13b28c79ee27be4b2e2ef5b902b2dd835cb96f16
Prashant <prashant.maroli@evolvussolutions.com>
2022-07-26 11:01:43 +0400

    gitignore changes

--- security.sh
+++
@@ -1,3 +0,0 @@
-export CLASSPATH=paycorp-security-1.0-SNAPSHOT.jar:commons-cli-1.4.jar:slf4j-api-1.7.32.jar:logback-classic-1.2.5.jar:logback-core-1.2.5.jar
-java com.paycorp.security.App $1 $2 $3 $4 $5 $6 $7 $8
-
